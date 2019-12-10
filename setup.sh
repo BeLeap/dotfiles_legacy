037d7b34b17f2d1cd022cf7b21a76044778aea05
BeLeap <ckdtj1012@naver.com>
2019-12-10 22:33:04 +0900

    setup.sh updated to install packages

--- setup.sh
+++ setup.sh
@@ -8,3 +8,14 @@ mkdir ~/.config/fish
 ln -s ./vim/.vimrc ~/.vimrc
 ln -s ./fish/config.fish ~/.config/fish/config.fish
 ln -s ./tmux/.tmux.conf ~/.tmux.conf
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y fish tmux vim
+
+chsh -s /usr/bin/fish $USER
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
+
+vim +PlugInstall +qall
