4a17ccae2e5b1c7c606a1149caec1264b76ef81c
BeLeap <ckdtj1012@naver.com>
2019-12-10 22:25:05 +0900

    README.md updated

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dotfiles
+
+```bash
+wget https://raw.githubusercontent.com/BeLeap/dotfiles/master/setup.sh && chmod +x ./setup.sh && ./setup.sh && rm ./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 
 git clone https://github.com/BeLeap/dotfiles ~/dotfiles
 
+mkdir ~/.config
+mkdir ~/.config/fish
+
 ln -s ./vim/.vimrc ~/.vimrc
 ln -s ./fish/config.fish ~/.config/fish/config.fish
 ln -s ./tmux/.tmux.conf ~/.tmux.conf
