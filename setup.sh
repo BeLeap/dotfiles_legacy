bdde0d35eb440c3f87f759edb819e2e359d97687
BeLeap <ckdtj1012@naver.com>
2019-12-10 21:36:40 +0900

    setup.sh updated to use more generally

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git clone https://github.com/BeLeap/dotfiles ~/dotfiles
+
 ln -s ./vim/.vimrc ~/.vimrc
 ln -s ./fish/config.fish ~/.config/fish/config.fish
 ln -s ./tmux/.tmux.conf ~/.tmux.conf
