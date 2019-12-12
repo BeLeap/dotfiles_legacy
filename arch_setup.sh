75ef1baaca08f0c1ffdd73e8fda58c77a12d0512
BeLeap <ckdtj1012@naver.com>
2019-12-12 16:06:58 +0900

    arch_setup.sh added

---
+++ arch_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git clone https://github.com/BeLeap/dotfiles ~/dotfiles
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+mkdir ~/.config
+mkdir ~/.config/fish
+
+ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.config
+
+sudo pacman -Syu
+sudo pacman -S fish tmux fd-find fzf
+
+chsh -s /usr/bin/fish $USER
+
+vim +PlugInstall +qall
+~/.tmux/plugins/tpm/bin/install_plugins
