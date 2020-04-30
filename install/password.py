f30ba364315ac23cee5d42cd439db205d77f081a
BeLeap <ckdtj1012@naver.com>
2020-04-30 21:49:17 +0900

    print prettier

--- install/password.py
+++ install/password.py
@@ -1,11 +1,14 @@
 import getpass
 
+from .bcolor import bcolors
+
 def get_passwd():
     password = getpass.getpass(prompt='Password: ', stream=None)
     return password
 
 def check_succeed(res):
     if res != 0:
+        print("\b" + bcolors.FAIL + "Execution Failed" + bcolors.ENDC)
         exit(1)
     else:
-        print("Executed successfully.")
+        print("\b" + bcolors.OKGREEN + "Executed Successfully." + bcolors.ENDC)
