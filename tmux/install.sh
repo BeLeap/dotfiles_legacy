8e0bb1c8fbc32fae951f11e61e8bafa8e87ebff8
BeLeap <ckdtj1012@naver.com>
2020-05-05 03:48:17 +0900

    install shell script removed

--- tmux/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "[LINK] tmux config"
-ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-echo "[INSTALL] tpm(git)"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo "[INSTALL] tmux plugins(tpm)"
-tinstall | fish
