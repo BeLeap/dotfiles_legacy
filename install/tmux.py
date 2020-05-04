160eb424d754034b775f3352bd9534f097ff69d3
BeLeap <ckdtj1012@naver.com>
2020-05-05 00:24:52 +0900

    tmux link deleted

--- install/tmux.py
+++ install/tmux.py
@@ -5,18 +5,6 @@ from .error import print_failed, print_succeed, check_success
 from .password import get_home
 from .spinner import Spinner
 
-def link_tmux_conf(dotdir, homedir):
-    print_header("LINK", "Linking tmux configuration file")
-    try:
-        os.symlink(dotdir + "/tmux/.tmux.conf", homedir + ".tmux.conf")
-    except:
-        try:
-            os.remove(homedir + ".tmux.conf")
-            os.symlink(dotdir + "/tmux/.tmux.conf", homedir + "/.tmux.conf")
-        except:
-            print_failed()
-    print_succeed()
-
 def install_tpm():
     res = os.system("git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm")
     check_success(res)
@@ -28,8 +16,6 @@ def install_tpm_plugins():
 def setup():
     homedir = get_home()
     dotdir = os.getcwd()
-    with Spinner():
-        link_tmux_conf(dotdir, homedir)
     with Spinner():
         install_tpm()
     with Spinner():
