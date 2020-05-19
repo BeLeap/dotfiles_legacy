d929459ceb8a7646d26dc26a4becccff6d818a93
BeLeap <ckdtj1012@naver.com>
2020-05-19 12:01:03 +0900

    add comments and postgres command

--- fish/alias.fish
+++ fish/alias.fish
@@ -1,21 +1,23 @@
-function v
-    nvim $argv
-end
+# Neovim
 function vim
     nvim $argv
+end
+
+# Short-handed commands
+function v
+    vim $argv
 end
 function vs
     vim src/$argv
 end
-
 function vm
     vim "main.$argv"
 end
-
 function c..
     cd ../
 end
 
+# Configuration Files
 function vimrc
     vim ~/.vimrc
 end
@@ -29,6 +31,7 @@ function tmuxconf
     vim ~/.tmux.conf
 end
 
+# Typo
 function clera
     clear
 end
@@ -36,6 +39,7 @@ function eixt
     exit
 end
 
+# Build and Run
 function ccr
     echo $argv > ~/.ccr.tmp
     set -l file (cut -d '.' -f1 ~/.ccr.tmp)
@@ -72,16 +76,7 @@ function npg
     npm $argv --prefix (git rev-parse --show-toplevel)
 end
 
-function rm
-    echo 'This is not a command what you intended to run.'; printf 'Please use'; echo (set_color green) 'trash-put' (set_color normal); printf 'If you want to execute it, use'; echo (set_color red) '/bin/rm' (set_color normal)
-end
-
-# for Windows Subsystem Linux
-function open
-    explorer.exe $argv
-end
-
-# Rust commands
+# Rust Commands
 alias ls "exa"
 alias ll "exa -l"
 alias lt "exa -l -T -L2"
@@ -96,3 +91,16 @@ alias grep "rg"
 alias tput "trash-put"
 alias tlist "trash-list";
 alias tempty "trash-empty"
+
+function rm
+    echo 'This is not a command what you intended to run.'; printf 'Please use'; echo (set_color green) 'trash-put' (set_color normal); printf 'If you want to execute it, use'; echo (set_color red) '/bin/rm' (set_color normal)
+end
+
+# for Windows Subsystem Linux
+function open
+    explorer.exe $argv
+end
+
+function psql
+    psql -U postgres -h localhost
+end
