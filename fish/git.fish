329488324586c46e962d817ad1969c5337e9e673
BeLeap <changseo_jang@korea.ac.kr>
2020-11-01 01:55:21 +0900

    Upgrade gacp

--- fish/git.fish
+++ fish/git.fish
@@ -3,7 +3,26 @@ function gitlog
 end
 
 function gacp
-    git add *; and git commit -m $argv; and git push
+    set -l inside_git_repo (git rev-parse --is-inside-work-tree 2>/dev/null)
+
+    if $inside_git_repo
+        git status
+
+        while true
+            read -p "echo 'Proceed (y/n)?'" -l confirm
+
+            switch $confirm
+            case Y y
+                git add .
+                read -p "echo 'Message?'" -l msg
+                git commit -m $msg
+                git push
+                return 0
+            case '' N n
+                return 1
+            end
+        end
+    end
 end
 
 function ggr
