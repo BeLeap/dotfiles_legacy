b530d194c1765537ea5e184d4563bff89b4ed037
BeLeap <changseo_jang@korea.ac.kr>
2021-10-04 00:28:58 +0900

    update

--- pwsh/Microsoft.Powershell_profile.ps1
+++ pwsh/Microsoft.Powershell_profile.ps1
@@ -66,19 +66,6 @@ function dock ($container) {
 Set-Alias touch New-Item
 Set-PSReadlineOption -EditMode Vi
 
-# function Prompt {
-#     $ansiEscape = [char]27
-#     $currentDirectory = Get-Location
-#     $currentBranch = git rev-parse --abbrev-ref HEAD
-#     $promptString = "$ansiEscape[34m$currentDirectory"
-#     if (-not ([string]::IsNullOrEmpty($currentBranch))) {
-#         $promptString += " $ansiEscape[90m($currentBranch)"
-#     }
-#     $promptString += "`r`n"
-#     $promptString += "$ansiEscape[35m" + [char]0x276f + "$ansiEscape[0m "
-#     return $promptString
-# }
-
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
@@ -86,101 +73,7 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 Invoke-Expression (&starship init powershell)
-
-# =============================================================================
-#
-# Utility functions for zoxide.
-#
-
-# pwd based on zoxide's format.
-function __zoxide_pwd {
-    $__zoxide_pwd = Get-Location
-    if ($__zoxide_pwd.Provider.Name -eq "FileSystem") {
-        $__zoxide_pwd.ProviderPath
-    }
-}
-
-# cd + custom logic based on the value of _ZO_ECHO.
-function __zoxide_cd($dir) {
-    Set-Location $dir -ea Stop
-}
-
-# =============================================================================
-#
-# Hook configuration for zoxide.
-#
-
-# Hook to add new entries to the database.
-function __zoxide_hook {
-    $__zoxide_result = __zoxide_pwd
-    if ($__zoxide_result -ne $null) {
-        zoxide add -- $__zoxide_result
-    }
-}
-
-# Initialize hook.
-if ($__zoxide_hooked -ne '1') {
-    $__zoxide_hooked = '1'
-    if ($PSVersionTable.PSVersion.Major -ge 6) {
-        $ExecutionContext.InvokeCommand.LocationChangedAction = {
-            $null = __zoxide_hook
-        }
-    } else {
-        Write-Error ("`n" +
-            "zoxide: PWD hooks are not supported below powershell 6.`n" +
-            "        Use 'zoxide init powershell --hook prompt' instead.")
-    }
-}
-
-# =============================================================================
-#
-# When using zoxide with --no-aliases, alias these internal functions as
-# desired.
-#
-
-# Jump to a directory using only keywords.
-function __zoxide_z {
-    if ($args.Length -eq 0) {
-        __zoxide_cd ~
-    }
-    elseif ($args.Length -eq 1 -and $args[0] -eq '-') {
-        __zoxide_cd -
-    }
-    elseif ($args.Length -eq 1 -and (Test-Path $args[0] -PathType Container)) {
-        __zoxide_cd $args[0]
-    }
-    else {
-        $__zoxide_result = __zoxide_pwd
-        if ($__zoxide_result -ne $null) {
-            $__zoxide_result = zoxide query --exclude $__zoxide_result -- @args
-        } else {
-            $__zoxide_result = zoxide query -- @args
-        }
-        if ($LASTEXITCODE -eq 0) {
-            __zoxide_cd $__zoxide_result
-        }
-    }
-}
-
-# Jump to a directory using interactive search.
-function __zoxide_zi {
-    $__zoxide_result = zoxide query -i -- @args
-    if ($LASTEXITCODE -eq 0) {
-        __zoxide_cd $__zoxide_result
-    }
-}
-
-# =============================================================================
-#
-# Convenient aliases for zoxide. Disable these using --no-aliases.
-#
-
-Set-Alias z __zoxide_z
-Set-Alias zi __zoxide_zi
-
-# =============================================================================
-#
-# To initialize zoxide, add this to your configuration (the location is stored
-# in $profile):
-#
-# Invoke-Expression (& { $hook = if ($PSVersionTable.PSVersion.Major -ge 6) { 'pwd' } else { 'prompt' } (zoxide init powershell --hook $hook) -join "`n" })
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell) -join "`n"
+})
