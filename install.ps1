0c9bcc949d88de792c118afe509aa96ef9715366
Changseo Jang <changseo_jang@korea.ac.kr>
2021-10-17 16:51:07 +0900

    fix

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
-New-Item -ItemType Directory -Path "$HOME\\OneDrive\\Documents\\WindowsPowershell"
-New-Item -ItemType SymbolicLink -Value "$HOME\\.dotfiles\\pwsh\\Microsoft.Powershell_profile.ps1" -Path "$HOME\\OneDrive\\Documents\\WindowsPowerShell\\Microsoft.Powershell_profile.ps1"
-New-Item -ItemType Directory -Path "$HOME\\OneDrive\\Documents\\Powershell"
-New-Item -ItemType SymbolicLink -Value "$HOME\\.dotfiles\\pwsh\\Microsoft.Powershell_profile.ps1" -Path "$HOME\\OneDrive\\Documents\\PowerShell\\Microsoft.Powershell_profile.ps1"
+New-Item -ItemType Directory -Path "$HOME\\Documents\\WindowsPowershell"
+New-Item -ItemType SymbolicLink -Value "$HOME\\.dotfiles\\pwsh\\Microsoft.Powershell_profile.ps1" -Path "$HOME\\Documents\\WindowsPowerShell\\Microsoft.Powershell_profile.ps1"
+New-Item -ItemType Directory -Path "$HOME\\Documents\\Powershell"
+New-Item -ItemType SymbolicLink -Value "$HOME\\.dotfiles\\pwsh\\Microsoft.Powershell_profile.ps1" -Path "$HOME\\Documents\\PowerShell\\Microsoft.Powershell_profile.ps1"
 
 New-Item -ItemType Directory -Path "$HOME\\.config"
 New-Item -ItemType SymbolicLink -Value "$HOME\\.dotfiles\\starship\\starship.toml" -Path "$HOME\\.config\\starship.toml"
