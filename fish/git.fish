f8d13ebcd77c2eee17a7a932b88ee67f381ab62b
Changseo Jang <changseo.jang@korea.ac.kr>
2022-02-03 09:07:09 +0900

    use own git abbr

--- fish/git.fish
+++ fish/git.fish
@@ -1,7 +1,3 @@
-function adog
-    git log --graph --decorate --oneline --all
-end
-
 function gacp
     set -l inside_git_repo (git rev-parse --is-inside-work-tree 2>/dev/null)
 
@@ -32,3 +28,13 @@ end
 function ggr
     cd (git rev-parse --show-toplevel)
 end
+
+abbr -a -g gco git checkout
+abbr -a -g ga git add
+abbr -a -g gst git status
+abbr -a -g gl git pull
+abbr -a -g gd git diff
+abbr -a -g gp git push
+abbr -a -g gc git commit -v
+abbr -a -g gf git fetch --prune --all
+abbr -a -g adog git log --graph --decorate --oneline --all
