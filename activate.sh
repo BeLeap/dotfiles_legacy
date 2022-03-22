95abcbfec9bbfa1b6f31b08371b0f7a73b1348c6
Changseo Jang <changseo.jang@mindslab.ai>
2022-03-22 10:57:29 +0900

    .

--- activate.sh
+++ activate.sh
@@ -10,10 +10,15 @@ deactivate() {
   unset FISH_CONFIG_PATH
 }
 
+# Fish shell
 dotfiles_path="$(cd $(dirname $BASH_SOURCE) &> /dev/null && pwd)"
 echo "$dotfiles_path"
 export XDG_CONFIG_HOME="$dotfiles_path"
 
+# Starship
+export STARSHIP_CONFIG="$dotfiles_path/starship/starship.toml"
+
+# Neovim
 if [ ! -d "$XDG_CONFIG_HOME/nvim/site/packer" ]
 then
   git clone --depth 1 https://github.com/wbthomason/packer.nvim $XDG_CONFIG_HOME/nvim/site/pack/packer/start/packer.nvim
