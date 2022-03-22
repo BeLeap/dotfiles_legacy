7c5fb7b8c115d76f4af23ec602d62f26a2bb2979
Changseo Jang <changseo.jang@mindslab.ai>
2022-03-22 17:14:08 +0900

    .

--- fish/config.fish
+++ fish/config.fish
@@ -21,9 +21,6 @@ if not set -q STARSHIP
   starship init fish | source
 end
 
-# Load nodenv automatically by appending
-# the following to ~/.config/fish/config.fish:
-
 if type -q nodenv
   status --is-interactive; and source (nodenv init -|psub)
 end
@@ -32,8 +29,5 @@ if type -q pyenv
   status is-login; and pyenv init --path | source
   status is-interactive; and pyenv init - | source
 end
-
-# Generated for envman. Do not edit.
-# test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/beleap/.ghcup/bin # ghcup-env
