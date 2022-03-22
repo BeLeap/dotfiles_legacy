dcbf481f8755b535c126095a565734bb985830ba
Changseo Jang <changseo.jang@mindslab.ai>
2022-03-22 10:06:35 +0900

    .

---
+++ activate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! command -v fish &> /dev/null
+then
+  echo "Fish is not installed. Please install it first."
+  exit 1
+fi
+
+deactivate() {
+  unset FISH_CONFIG_PATH
+}
+
+export FISH_CONFIG_PATH="${BASH_SOURCE}/fish"
