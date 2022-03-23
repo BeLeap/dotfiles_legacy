dfd2f6e3a8347c4b108a79596a87fc6b953b6f04
Changseo Jang <changseo.jang@korea.ac.kr>
2022-03-23 20:55:41 +0900

    .

--- activate.fish
+++ activate.fish
@@ -1,7 +1,3 @@
-# Fish shell
-set -gx DOTFILES_PATH (realpath (dirname $argv[1]))
-echo $DOTFILES_PATH
-
 function deactivate -d "Exit"
   if test -n "$__OLD_XDG_CONFIG_HOME"
     set -gx XDG_CONFIG_HOME $__OLD_XDG_CONFIG_HOME
@@ -23,20 +19,23 @@ function deactivate -d "Exit"
   functions -e deactivate
 end
 
+# Fish shell
+set -gx DOTFILES_PATH (realpath (dirname $argv[1]))
+
 set -gx __OLD_XDG_CONFIG_HOME $XDG_CONFIG_HOME
 set -gx XDG_CONFIG_HOME $DOTFILES_PATH
 
 # Starship
 set -gx __OLD_STARSHIP_CONFIG $STARSHIP_CONFIG
-set -gx STARSHIP_CONFIG $DOTFILES_PATH"/starship/starship.toml"
+set -gx STARSHIP_CONFIG "$DOTFILES_PATH/starship/starship.toml"
 
 # Neovim
-if test -d $XDG_CONFIG_HOME"/nvim/site/pack/packer"
+if ! test -d "$XDG_CONFIG_HOME/nvim/site/pack/packer"
   git clone --depth 1 https://github.com/wbthomason/packer.nvim $XDG_CONFIG_HOME/nvim/site/pack/packer/start/packer.nvim
 end
 
 # pyenv
 set -gx __OLD_PYENV_ROOT $PYENV_ROOT
-set -gx PYENV_ROOT $DOTFILES_PATH"/pyenv"
+set -gx PYENV_ROOT "$DOTFILES_PATH/pyenv"
 set -gx __OLD_PATH $PATH
-set -gx PATH $PYENV_ROOT"/bin" $PATH
+set -gx PATH "$PYENV_ROOT/bin" $PATH
