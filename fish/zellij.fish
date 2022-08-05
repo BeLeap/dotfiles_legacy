b57a7ea72486804519d6e3cbd8d7f11ebb408831
Changseo Jang <changseo.jang@mindslab.ai>
2022-08-05 11:13:44 +0900

    .

--- fish/zellij.fish
+++ fish/zellij.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+and not set -q INTEGRATED_TERMINAL
   set ZELLIJ_AUTO_ATTACH true
   set ZELLIJ_AUTO_EXIT true
   eval (zellij setup --generate-auto-start fish | string collect)
