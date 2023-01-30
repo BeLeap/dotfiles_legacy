14ea0ff286223eb4fdec9f83827c9a8276301494
Changseo Jang <changseo.jang@riiid.co>
2023-01-30 13:25:37 +0900

    .

--- nvim/lua/others.lua
+++ nvim/lua/others.lua
@@ -101,4 +101,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  sync_root_with_cwd = true,
+})
+
