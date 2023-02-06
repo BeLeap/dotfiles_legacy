0a1abb0ecd68a4642f300471d5075c76e1ac7261
Changseo Jang <changseo.jang@riiid.co>
2023-02-06 09:41:08 +0900

    .

--- fish/zellij.fish
+++ fish/zellij.fish
@@ -1,5 +1,7 @@
 if status is-interactive
 and not set -q INTEGRATED_TERMINAL
+  set -gx ZELLIJ_AUTO_ATTACH true
+  set -gx ZELLIJ_AUTO_EXIT true
   eval (zellij setup --generate-auto-start fish | string collect)
 end
 
