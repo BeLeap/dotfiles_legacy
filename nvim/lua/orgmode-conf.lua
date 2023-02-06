12b16cab953e0d5b13ecd45839c1949cbdd1324c
Changseo Jang <changseo.jang@riiid.co>
2023-02-06 16:07:32 +0900

    [neovim] use todo.org as default

--- nvim/lua/orgmode-conf.lua
+++ nvim/lua/orgmode-conf.lua
@@ -2,6 +2,6 @@ require('orgmode').setup_ts_grammar()
 
 require('orgmode').setup({
   org_agenda_files = { '~/riiid/**/*.org' },
-  org_default_notes_file = '~/riiid/org/refile.org',
+  org_default_notes_file = '~/riiid/org/todo.org',
   org_agenda_start_on_weekday = 7,
 })
