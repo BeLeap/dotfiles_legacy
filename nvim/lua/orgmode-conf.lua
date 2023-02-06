61bf69879f04c0472405a2113aba5b8bf2160d29
Changseo Jang <changseo.jang@riiid.co>
2023-02-06 16:06:32 +0900

    [neovim] fix orgmode config

--- nvim/lua/orgmode-conf.lua
+++ nvim/lua/orgmode-conf.lua
@@ -1,6 +1,7 @@
 require('orgmode').setup_ts_grammar()
 
 require('orgmode').setup({
-  org_agenda_files = { '~/riiid/**/*' },
+  org_agenda_files = { '~/riiid/**/*.org' },
   org_default_notes_file = '~/riiid/org/refile.org',
+  org_agenda_start_on_weekday = 7,
 })
