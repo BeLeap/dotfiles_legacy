38bede843cc13fb292e255064e86a95eafa025c6
BeLeap <beleap@beleap.codes>
2023-02-06 23:01:12 +0900

    [neovim] add omitted conf file

---
+++ nvim/lua/git-conf.lua
@@ -0,0 +1,17 @@
+require('octo').setup()
+require('git').setup({
+  keymaps = {
+    blame = "<Leader>gb",
+    quit_blame = "q",
+    blame_commit = "<CR>",
+    browse = "<Leader>go",
+    open_pull_request = "<Leader>gp",
+    create_pull_request = "<Leader>gn",
+    diff = "<Leader>gd",
+    diff_close = "<Leader>gD",
+    revert = "<Leader>gr",
+    revert_file = "<Leader>gR",
+  },
+  target_branch = "main",
+})
+
