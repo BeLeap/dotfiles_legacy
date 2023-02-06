51799e13429e115047982339632416a780bb0a08
BeLeap <beleap@beleap.codes>
2023-02-06 22:41:04 +0900

    [neovim] remove chadtree configuration

--- nvim/lua/boot.lua
+++ nvim/lua/boot.lua
@@ -9,7 +9,5 @@ require('dap-conf')
 require('lsp')
 require('telescope-conf')
 require('line-conf')
-require('chadtree-conf')
-
 require('nvimtree-conf')
 
--- nvim/lua/chadtree-conf.lua
+++
@@ -1,5 +0,0 @@
-local chadtree_settings = {
-  ["theme.text_colour_set"] = "trapdoor"
-}
-
-vim.api.nvim_set_var("chadtree_settings", chadtree_settings)
