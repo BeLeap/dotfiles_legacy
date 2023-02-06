ebf2dc023273d27b53df9d908a74ea35010953de
Changseo Jang <changseo.jang@riiid.co>
2023-02-06 09:46:43 +0900

    open in maximzed

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,10 @@
 local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+  local _tab, _pane, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
 
 return {
   font = wezterm.font 'Caskaydia Cove Nerd Font',
