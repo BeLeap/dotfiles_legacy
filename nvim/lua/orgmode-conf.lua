a82b0aa51b8d6b08fa4877dffc2397f5eb4b0b02
Changseo Jang <changseo.jang@riiid.co>
2023-02-07 10:13:16 +0900

    [neovim] better org default file handling

--- nvim/lua/orgmode-conf.lua
+++ nvim/lua/orgmode-conf.lua
@@ -1,7 +1,14 @@
 require("orgmode").setup_ts_grammar()
 
+local org_default_notes_file = (function()
+	if string.match(vim.fn.getcwd(), "riiid") then
+		return "~/orgmode/riiid/todo.org"
+	else
+		return "~/orgmode/personal/todo.org"
+	end
+end)()
+
 require("orgmode").setup({
-	org_agenda_files = { "~/orgmode/*.org" },
-	org_default_notes_file = "~/orgmode/todo.org",
-	org_agenda_start_on_weekday = 7,
+	org_agenda_files = { "~/orgmode/**/*.org" },
+	org_default_notes_file = org_default_notes_file,
 })
