65e2b976a53e15fc5a9787c6e81b342b9df7a753
BeLeap <beleap@beleap.codes>
2023-02-22 00:03:48 +0900

    [neovim] windows shell support

--- nvim/lua/toggleterm-conf.lua
+++ nvim/lua/toggleterm-conf.lua
@@ -1,3 +1,18 @@
+if vim.loop.os_uname().sysname == "Windows_NT" then
+	local powershell_options = {
+		shell = vim.fn.executable("pwsh") and "pwsh" or "powershell",
+		shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+		shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+		shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+		shellquote = "",
+		shellxquote = "",
+	}
+
+	for option, value in pairs(powershell_options) do
+		vim.opt[option] = value
+	end
+end
+
 vim.keymap.set("n", "<leader>bt", function()
 	local terminal = require("toggleterm.terminal")
 	local bufferWd = vim.fn.expand("%:p:h")
