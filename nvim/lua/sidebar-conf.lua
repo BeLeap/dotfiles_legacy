150cb283106e47655379b8a935871b17a1140c60
Changseo Jang <changseo.jang@riiid.co>
2023-02-21 19:41:30 +0900

    [neovim] refresh periodically

--- nvim/lua/sidebar-conf.lua
+++ nvim/lua/sidebar-conf.lua
@@ -18,3 +18,12 @@ require("sidebar-nvim").setup({
 		"todos",
 	},
 })
+
+local timer = vim.loop.new_timer()
+timer:start(
+	1000,
+	1000,
+	vim.schedule_wrap(function()
+		require("sidebar-nvim").update()
+	end)
+)
