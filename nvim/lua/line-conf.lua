b481e494e8fe37c30c4768b5d28a6b8f8a238935
Changseo Jang <changseo.jang@riiid.co>
2023-02-21 18:40:08 +0900

    [neovim] show full path in lualine

--- nvim/lua/line-conf.lua
+++ nvim/lua/line-conf.lua
@@ -2,6 +2,14 @@ require("lualine").setup({
 	options = {
 		theme = "gruvbox-material",
 	},
+	sections = {
+		lualine_c = {
+			{
+				"filename",
+				path = 1,
+			},
+		},
+	},
 })
 
 require("tabline").setup({
