0eb7b150a9e5c258459f6fbb296db01fb43733df
Changseo Jang <changseo.jang@riiid.co>
2023-02-24 17:55:03 +0900

    [neovim] revise toggleterm config

--- nvim/lua/toggleterm-conf.lua
+++ nvim/lua/toggleterm-conf.lua
@@ -13,33 +13,13 @@ if vim.loop.os_uname().sysname == "Windows_NT" then
 	end
 end
 
-vim.keymap.set("n", "<leader>bt", function()
-	local terminal = require("toggleterm.terminal")
-	local bufferWd = vim.fn.expand("%:p:h")
-	local bufferTerminal = terminal.get_or_create_term(1, bufferWd)
+vim.keymap.set("n", "<leader>t", ":ToggleTerm<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", [[<c-\>]], ":ToggleTerm<cr>", { noremap = true, silent = true })
 
-	vim.keymap.set(
-		"t",
-		",c",
-		"<c-\\><c-N>:ToggleTerm<cr>",
-		{ silent = true, noremap = true, buffer = bufferTerminal.bufnr }
-	)
-	vim.keymap.set("t", ",d", "<c-\\><c-N>", { silent = true, noremap = true, buffer = bufferTerminal.bufnr })
+function _G.set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", ",d", [[<c-\><c-n>]], opts)
+	vim.keymap.set("t", ",c", [[<c-\><c-n>:ToggleTerm<cr>]], opts)
+end
 
-	local size = vim.api.nvim_win_get_height(0)
-	bufferTerminal:toggle(size / 2)
-end, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>t", function()
-	local terminal = require("toggleterm.terminal")
-	local wdTerminal = terminal.get_or_create_term(1)
-
-	vim.keymap.set(
-		"t",
-		",d",
-		"<c-\\><c-N>:ToggleTerm<cr>",
-		{ silent = true, noremap = true, buffer = wdTerminal.bufnr }
-	)
-
-	local size = vim.api.nvim_win_get_height(0)
-	wdTerminal:toggle(size / 2)
-end, { noremap = true, silent = true })
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
