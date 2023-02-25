f04ccbee1fe7d726cc09a194a364dc120a23ab21
BeLeap <beleap@beleap.codes>
2023-02-25 22:31:05 +0900

    [neovim] sidebar.nvim is un-layzable

--- nvim/lua/sidebar-conf.lua
+++ nvim/lua/sidebar-conf.lua
@@ -1,35 +1,47 @@
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
 
-vim.keymap.set("n", "<leader>i", function()
-	require("sidebar-nvim").toggle()
-end, { noremap = true, silent = true })
+return {
+	"sidebar-nvim/sidebar.nvim",
+	keys = {
+		{
+			"<leader>e",
+			function()
+				require("sidebar-nvim").toggle()
+			end,
+			noremap = true,
+			silent = true,
+		},
+	},
+	lazy = false,
+	config = function()
+		require("sidebar-nvim").setup({
+			open = true,
+			bindings = {
+				["<c-r>"] = function()
+					require("sidebar-nvim").update()
+				end,
+			},
+			sections = {
+				"buffers",
+				"git",
+				"files",
+				"symbols",
+				"diagnostics",
+				"todos",
+			},
+			files = {
+				show_hidden = true,
+			},
+		})
 
-require("sidebar-nvim").setup({
-	open = true,
-	bindings = {
-		["<c-r>"] = function()
-			require("sidebar-nvim").update()
-		end,
-	},
-	sections = {
-		"buffers",
-		"git",
-		"files",
-		"symbols",
-		"diagnostics",
-		"todos",
-	},
-	files = {
-		show_hidden = true,
-	},
-})
-
-local timer = vim.loop.new_timer()
-timer:start(
-	1000,
-	1000,
-	vim.schedule_wrap(function()
-		require("sidebar-nvim").update()
-	end)
-)
+		-- local timer = vim.loop.new_timer()
+		-- timer:start(
+		--   1000,
+		--   1000,
+		--   vim.schedule_wrap(function()
+		--     require("sidebar-nvim").update()
+		--   end)
+		-- )
+	end,
+}
