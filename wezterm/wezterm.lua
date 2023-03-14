93320754511468a45ba2cdf94bfff63972c7f45e
Changseo Jang <changseo.jang@riiid.co>
2023-03-14 10:03:36 +0900

    [wezterm] change theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,14 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local mux = wezterm.mux
 
 wezterm.on("gui-startup", function()
-  local _tab, _pane, window = mux.spawn_window{}
-  window:gui_window():maximize()
+	local _tab, _pane, window = mux.spawn_window({})
+	window:gui_window():maximize()
 end)
 
 return {
-  font = wezterm.font 'Caskaydia Cove Nerd Font',
-  font_size = 14.0,
-  color_scheme = "Gruvbox dark, medium (base16)",
-  enable_tab_bar = false,
+	font = wezterm.font("Caskaydia Cove Nerd Font"),
+	font_size = 14.0,
+	color_scheme = "Catppuccin Mocha",
+	enable_tab_bar = false,
 }
