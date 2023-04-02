08367585ef17582773eb3b506695737e2b73b1f8
BeLeap <beleap@beleap.dev>
2023-04-03 00:09:05 +0900

    add stow makefile

---
+++ Makefile
@@ -0,0 +1,8 @@
+stow:
+	stow fish
+	stow k9s
+	stow nvim
+	stow starship
+	stow sway
+	stow wezterm
+	stow zellij
--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt install neovim curl build-essential -y
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-PATH=$HOME/.cargo/bin:$PATH cargo install zoxide zellij lsd bat hexyl starship
