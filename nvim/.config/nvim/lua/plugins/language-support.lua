6fbba2cbf9c0b08522fb90423babb8a3adece36b
BeLeap <beleap@beleap.dev>
2023-04-04 14:47:36 +0900

    [neovim] mason add cmd

--- nvim/.config/nvim/lua/plugins/language-support.lua
+++ nvim/.config/nvim/lua/plugins/language-support.lua
@@ -76,6 +76,14 @@ return {
 	},
 	{
 		"williamboman/mason.nvim",
+		cmd = {
+			"Mason",
+			"MasonUpdate",
+			"MasonInstall",
+			"MasonUninstall",
+			"MasonUninstallAll",
+			"MasonLog",
+		},
 		config = function()
 			require("mason").setup()
 		end,
