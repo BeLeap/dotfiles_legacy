9e920892843ebd1073cec662a0b63c9f67faa7e3
BeLeap <beleap@beleap.dev>
2023-04-24 15:55:45 +0900

    .

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -1,32 +1,32 @@
 -- Replace orgmode when GTD done
 return {
-  {
-    "nvim-neorg/neorg",
-    ft = "norg",
-    cmd = "Neorg",
-    build = ":Neorg sync-parsers",
-    opts = {
-      load = {
-        ["core.defaults"] = {},
-        ["core.concealer"] = {},
-        ["core.dirman"] = {
-          config = {
-            workspaces = {
-              work = "~/notes/work",
-              personal = "~/notes/personal",
-            },
-          },
-        },
-        ["core.completion"] = {
-          config = {
-            engine = "nvim-cmp",
-          },
-        },
-      },
-    },
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-      { "hrsh7th/nvim-cmp" },
-    },
-  },
+	{
+		"nvim-neorg/neorg",
+		ft = "norg",
+		cmd = { "Neorg", "Norg" },
+		build = ":Neorg sync-parsers",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "hrsh7th/nvim-cmp" },
+		},
+		opts = {
+			load = {
+				["core.defaults"] = {},
+				["core.concealer"] = {},
+				["core.dirman"] = {
+					config = {
+						workspaces = {
+							work = "~/notes/work",
+							personal = "~/notes/personal",
+						},
+					},
+				},
+				["core.completion"] = {
+					config = {
+						engine = "nvim-cmp",
+					},
+				},
+			},
+		},
+	},
 }
