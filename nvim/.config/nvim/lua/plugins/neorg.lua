88ef449a6e17f0d3fcab86a363d601f6674415e8
BeLeap <beleap@beleap.dev>
2023-04-24 14:34:27 +0900

    [neovim] chore: update deprecated configs

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -1,32 +1,32 @@
 -- Replace orgmode when GTD done
 return {
-	{
-		"nvim-neorg/neorg",
-		ft = "norg",
-		cmd = "Neorg",
-		build = ":Neorg sync-parsers",
-		opts = {
-			load = {
-				["core.defaults"] = {},
-				["core.norg.concealer"] = {},
-				["core.norg.dirman"] = {
-					config = {
-						workspaces = {
-							riiid = "~/notes/riiid",
-							personal = "~/notes/personal",
-						},
-					},
-				},
-				["core.norg.completion"] = {
-					config = {
-						engine = "nvim-cmp",
-					},
-				},
-			},
-		},
-		dependencies = {
-			{ "nvim-lua/plenary.nvim" },
-			{ "hrsh7th/nvim-cmp" },
-		},
-	},
+  {
+    "nvim-neorg/neorg",
+    ft = "norg",
+    cmd = "Neorg",
+    build = ":Neorg sync-parsers",
+    opts = {
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              work = "~/notes/work",
+              personal = "~/notes/personal",
+            },
+          },
+        },
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        },
+      },
+    },
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "hrsh7th/nvim-cmp" },
+    },
+  },
 }
