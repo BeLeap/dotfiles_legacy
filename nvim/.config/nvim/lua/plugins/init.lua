3e43e3dd75b68226fb48bdf4d19d0e1199ab3f71
BeLeap <beleap@beleap.dev>
2023-05-02 23:20:52 +0900

    neovim: remove wilder

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -73,49 +73,6 @@ return {
 			vim.notify = notify
 		end,
 	},
-	{
-		"gelguy/wilder.nvim",
-		event = "VeryLazy",
-		dependencies = { "romgrk/fzy-lua-native" },
-		config = function()
-			local wilder = require("wilder")
-			wilder.setup({ modes = { ":", "/", "?" } })
-			-- Disable Python remote plugin
-			wilder.set_option("use_python_remote_plugin", 0)
-
-			wilder.set_option("pipeline", {
-				wilder.branch(
-					wilder.cmdline_pipeline({
-						fuzzy = 1,
-						fuzzy_filter = wilder.lua_fzy_filter(),
-					}),
-					wilder.vim_search_pipeline()
-				),
-			})
-
-			wilder.set_option(
-				"renderer",
-				wilder.popupmenu_renderer({
-					highlighter = wilder.lua_fzy_highlighter(),
-					left = {
-						" ",
-						wilder.popupmenu_devicons(),
-					},
-					right = {
-						" ",
-						wilder.popupmenu_scrollbar(),
-					},
-					highlights = {
-						accent = wilder.make_hl(
-							"WilderAccent",
-							"Pmenu",
-							{ { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }
-						),
-					},
-				})
-			)
-		end,
-	},
 
 	-- Language
 	{ "lbrayner/vim-rzip" },
