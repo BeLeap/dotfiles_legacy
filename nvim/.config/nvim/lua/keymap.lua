0d322902a42f43305999f38563c28bc2ca7fabd6
BeLeap <beleap@beleap.dev>
2023-05-08 09:57:33 +0900

    neovim: update keymap

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -3,8 +3,8 @@ vim.keymap.set("n", "<C-q>", ":qa<cr>", { noremap = true, silent = true })
 vim.keymap.set("i", "<leader>q", "<esc>:bd<cr>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>q", ":bd<cr>", { noremap = true, silent = true })
 
-vim.keymap.set("i", "<C-s>", "<esc>:update<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-s>", ":update<cr>", { noremap = true, silent = true })
+vim.keymap.set("i", "<leader>s", "<esc>:update<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>s", ":update<cr>", { noremap = true, silent = true })
 
 vim.keymap.set("i", "<Tab>", 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { expr = true, noremap = true, silent = true })
 vim.keymap.set("i", "<S-Tab>", 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', { expr = true, noremap = true, silent = true })
