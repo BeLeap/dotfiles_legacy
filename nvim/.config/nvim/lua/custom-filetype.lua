106a978a3b102fba0709bc9c25e5fa718d72b5be
BeLeap <beleap@beleap.dev>
2023-08-28 15:33:58 +0900

    better helm filetype handling

--- nvim/.config/nvim/lua/custom-filetype.lua
+++ nvim/.config/nvim/lua/custom-filetype.lua
@@ -4,7 +4,20 @@ vim.filetype.add({
 			priority = 1,
 			function(path)
 				if string.find(path, "templates") then
-					return "helm"
+					local parent = vim.split(path, "templates")[1]
+					local files_raw = vim.fn.system("ls " .. parent)
+					local files = vim.split(files_raw, "\n")
+
+					local is_helm = false
+					for _, v in ipairs(files) do
+						if v == "Chart.yaml" then
+							is_helm = true
+						end
+					end
+
+					if is_helm then
+						return "helm"
+					end
 				elseif string.find(path, "playbook") then
 					return "yaml.ansible"
 				end
