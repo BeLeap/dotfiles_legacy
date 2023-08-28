cef545395614befd3fbf0983a1474f75161cc153
BeLeap <beleap@beleap.dev>
2023-08-28 15:38:14 +0900

    handle helm tpl

--- nvim/.config/nvim/lua/custom-filetype.lua
+++ nvim/.config/nvim/lua/custom-filetype.lua
@@ -24,7 +24,24 @@ vim.filetype.add({
 			end,
 		},
 		[".*%.tpl"] = {
-			"yaml.tpl",
+			function(path)
+				if string.find(path, "templates") then
+					local parent = vim.split(path, "templates")[1]
+					local files_raw = vim.fn.system("ls " .. parent)
+					local files = vim.split(files_raw, "\n")
+
+					local is_helm = false
+					for _, v in ipairs(files) do
+						if v == "Chart.yaml" then
+							is_helm = true
+						end
+					end
+
+					if is_helm then
+						return "helm"
+					end
+				end
+			end,
 		},
 		[".*mdx"] = {
 			"markdown",
