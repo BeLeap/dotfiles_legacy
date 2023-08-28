e3fef3a0d0c6f3ba459d0dbdc78da3c1716abc6d
BeLeap <beleap@beleap.dev>
2023-08-28 15:40:17 +0900

    refact: modulize common function

--- nvim/.config/nvim/lua/custom-filetype.lua
+++ nvim/.config/nvim/lua/custom-filetype.lua
@@ -1,23 +1,28 @@
+local is_helm = function(path)
+	local is_helm = false
+
+	if string.find(path, "templates") then
+		local parent = vim.split(path, "templates")[1]
+		local files_raw = vim.fn.system("ls " .. parent)
+		local files = vim.split(files_raw, "\n")
+
+		for _, v in ipairs(files) do
+			if v == "Chart.yaml" then
+				is_helm = true
+			end
+		end
+	end
+
+	return is_helm
+end
+
 vim.filetype.add({
 	pattern = {
 		[".*yaml"] = {
 			priority = 1,
 			function(path)
-				if string.find(path, "templates") then
-					local parent = vim.split(path, "templates")[1]
-					local files_raw = vim.fn.system("ls " .. parent)
-					local files = vim.split(files_raw, "\n")
-
-					local is_helm = false
-					for _, v in ipairs(files) do
-						if v == "Chart.yaml" then
-							is_helm = true
-						end
-					end
-
-					if is_helm then
-						return "helm"
-					end
+				if is_helm(path) then
+					return "helm"
 				elseif string.find(path, "playbook") then
 					return "yaml.ansible"
 				end
@@ -25,21 +30,8 @@ vim.filetype.add({
 		},
 		[".*%.tpl"] = {
 			function(path)
-				if string.find(path, "templates") then
-					local parent = vim.split(path, "templates")[1]
-					local files_raw = vim.fn.system("ls " .. parent)
-					local files = vim.split(files_raw, "\n")
-
-					local is_helm = false
-					for _, v in ipairs(files) do
-						if v == "Chart.yaml" then
-							is_helm = true
-						end
-					end
-
-					if is_helm then
-						return "helm"
-					end
+				if is_helm(path) then
+					return "helm"
 				end
 			end,
 		},
