045d6aadfba0deb117ffc9c11205ae76e4179991
BeLeap <beleap@beleap.dev>
2023-09-04 08:15:09 +0900

    fix condition for dev loading

--- nvim/.config/nvim/lua/plugins/kube.lua
+++ nvim/.config/nvim/lua/plugins/kube.lua
@@ -1,12 +1,12 @@
 return {
-	{
-		"BeLeap/k8s-lua.nvim",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-telescope/telescope.nvim",
-		},
-		cmd = { "Kube", "KubeApply" },
-		config = true,
-		dev = not string.find(vim.loop.cwd(), "riiid"),
-	},
+  {
+    "BeLeap/k8s-lua.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = { "Kube", "KubeApply" },
+    config = true,
+    dev = not string.find(vim.loop.cwd(), "work"),
+  },
 }
