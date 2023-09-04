f5db0a0e400cb01c6765396ce6c6297ca0e92d80
BeLeap <beleap@beleap.dev>
2023-09-04 14:45:11 +0900

    add pane focus move keymap

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -8,3 +8,8 @@ vim.keymap.set("i", "<Tab>", 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { expr = tr
 vim.keymap.set("i", "<S-Tab>", 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', { expr = true, noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>u", "<esc>:undo<cr>", { silent = true, desc = "Undo" })
+
+vim.keymap.set("n", "<M-h>", "<C-w>h", { desc = "Move focus to left" })
+vim.keymap.set("n", "<M-j>", "<C-w>j", { desc = "Move focus to down" })
+vim.keymap.set("n", "<M-k>", "<C-w>k", { desc = "Move focus to up" })
+vim.keymap.set("n", "<M-l>", "<C-w>l", { desc = "Move focus to right" })
