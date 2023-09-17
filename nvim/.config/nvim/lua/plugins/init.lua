25b764b19640af89d81cc741c3a41a1a4ae94164
BeLeap <beleap@beleap.dev>
2023-09-17 11:56:22 +0900

    fix: space checker

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -117,7 +117,7 @@ return {
 			vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
 
 			vim.opt.list = true
-			vim.opt.listchars:append("space:⋅")
+			vim.opt.listchars:append("space:-")
 			require("indent_blankline").setup({
 				space_char_blankline = " ",
 				char_highlight_list = {
