1230542c7a5777cdf8e50a3187b015faab002123
Changseo Jang <cs.jang@toss.im>
2024-03-05 21:02:59 +0900

    feat: setup abbrs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,5 +6,20 @@ if status is-interactive
 
     alias sofish "source ~/.config/fish/config.fish"
 
+    abbr gst "git status"
+    abbr gsw "git switch"
+    abbr gd "git diff"
+    abbr ga "git add"
+    abbr gc "git commit -v"
+    abbr gac "git commit -av"
+    abbr gp "git push"
+    abbr gf "git fetch --prune --all"
+    abbr gl "git pull"
+    abbr gco "git checkout"
+
+    abbr k "kubectl"
+    abbr ktx "kubectx"
+    abbr kns "kubens"
+
     starship init fish | source
 end
