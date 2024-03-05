949ef555e382a78a6152b767a3a59dfec61b1fad
Changseo Jang <cs.jang@toss.im>
2024-03-05 20:55:36 +0900

    feat: setup wezterm

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 .PHONY: tui gui
 
+all: tui gui
+
 tui:
 	@stow kubernetes
 	@stow nvim
 	@stow fish
 	@stow starship
+
+gui:
+	@stow wezterm
---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,6 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'CaskaydiaMono Nerd Font Mono'
+
+return config
