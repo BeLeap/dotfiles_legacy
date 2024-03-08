0b22e390261e3c8b2c9777fbdbb715c1c0929621
BeLeap <beleap@beleap.dev>
2024-03-08 09:58:42 +0900

    increase wezterm font size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font 'CaskaydiaMono Nerd Font Mono'
+config.font_size = 16.0
 config.color_scheme = 'nord'
 config.enable_tab_bar = false
 
