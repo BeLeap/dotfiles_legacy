58eac10c4237074453de9e3ed14b6bd65be2b0ba
BeLeap <beleap@beleap.dev>
2024-04-15 14:17:27 +0900

    format

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,63 +1,63 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    fish_vi_key_bindings
+  # Commands to run in interactive sessions can go here
+  fish_vi_key_bindings
 
-    set -x EDITOR nvim
+  set -x EDITOR nvim
 
-    alias sofish "source ~/.config/fish/config.fish"
-    alias docker "podman"
+  alias sofish "source ~/.config/fish/config.fish"
+  alias docker "podman"
 
-    abbr v "nvim"
+  abbr v "nvim"
 
-    abbr gst "git status"
-    abbr gsw "git switch"
-    abbr gd "git diff"
-    abbr ga "git add"
-    abbr gc "git commit -v"
-    abbr gac "git commit -av"
-    abbr gp "git push"
-    abbr gf "git fetch --prune --all"
-    abbr gl "git pull"
-    abbr gco "git checkout"
+  abbr gst "git status"
+  abbr gsw "git switch"
+  abbr gd "git diff"
+  abbr ga "git add"
+  abbr gc "git commit -v"
+  abbr gac "git commit -av"
+  abbr gp "git push"
+  abbr gf "git fetch --prune --all"
+  abbr gl "git pull"
+  abbr gco "git checkout"
 
-    abbr k "kubectl"
-    abbr ktx "kubectx"
-    abbr kns "kubens"
+  abbr k "kubectl"
+  abbr ktx "kubectx"
+  abbr kns "kubens"
 
-    abbr prmd "gh pr merge -d"
-    abbr prvw "gh pr view -w"
-    abbr prv "gh pr view"
-    abbr prcm "gh pr create --assignee @me"
+  abbr prmd "gh pr merge -d"
+  abbr prvw "gh pr view -w"
+  abbr prv "gh pr view"
+  abbr prcm "gh pr create --assignee @me"
 
-    if type -q starship 
-        starship init fish | source
-    end
-    if type -q zoxide
-        zoxide init fish | source
-    end
-    if type -q lsd
-        alias ls "lsd"
-        alias ll "lsd -l"
-        alias lla "lsd -la"
-    end
+  if type -q starship 
+    starship init fish | source
+  end
+  if type -q zoxide
+    zoxide init fish | source
+  end
+  if type -q lsd
+    alias ls "lsd"
+    alias ll "lsd -l"
+    alias lla "lsd -la"
+  end
 
-    if type -q nodenv
-        source (nodenv init -|psub)
-    end
+  if type -q nodenv
+    source (nodenv init -|psub)
+  end
 
-    if type -q direnv
-        direnv hook fish | source
-    end
+  if type -q direnv
+    direnv hook fish | source
+  end
 
-    if type -q carapace
-        carapace _carapace | source
-    end
+  if type -q carapace
+    carapace _carapace | source
+  end
 
-    if type -q rbenv
-        status --is-interactive; and rbenv init - fish | source
-    end
+  if type -q rbenv
+    status --is-interactive; and rbenv init - fish | source
+  end
 
-    if type -q fzf
-        fzf --fish | source
-    end
+  if type -q fzf
+    fzf --fish | source
+  end
 end
