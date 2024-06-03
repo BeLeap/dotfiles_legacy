39223b2010fe9ebd009f00f3a4cd1373bb426970
BeLeap <beleap@beleap.dev>
2024-06-03 16:19:30 +0900

    .

--- neovim/.config/nvim/lua/plugins/nvim-ufo.lua
+++ neovim/.config/nvim/lua/plugins/nvim-ufo.lua
@@ -5,11 +5,12 @@ return {
     dependencies = {
       "kevinhwang91/promise-async",
       "nvim-treesitter/nvim-treesitter",
+      "neovim/nvim-lspconfig",
     },
     config = function()
       vim.o.foldcolumn = "1"
-      vim.o.foldlevel = 999
-      vim.o.foldlevelstart = 99
+      vim.o.foldlevel = 9999
+      vim.o.foldlevelstart = 9999
 
       vim.keymap.set("n", "zR", require("ufo").openAllFolds)
       vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
